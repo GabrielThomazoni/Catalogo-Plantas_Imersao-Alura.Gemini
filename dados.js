5a227adf4b1b06b287e58ee3d756744f8440fafd
GabrielThomazoni <72112905+GabrielThomazoni@users.noreply.github.com>
2024-09-07 00:34:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+        titulo : "Dama da Noite",
+        descricao : "A Dama da Noite (Epiphyllum oxypetalum) é uma planta famosa por sua exuberante florada noturna e perfume intenso. Pertencente à família dos cactos, ela é uma ótima opção para quem busca uma planta exótica e fácil de cuidar.",
+        luz : "Meia-sombra",
+        rega : "Moderada, evitando encharcamento",
+        temperatura : "Quente e úmida",
+        solo : "Rico em matéria orgânica, bem drenado",
+        adubacao : "A cada 2 meses, adubo rico em fósforo",
+        poda : "Após a floração",
+        moreinfo : "https://pt.wikipedia.org/wiki/Epiphyllum",
+        tags: "noturna branca cacto"
+    },
+    {
+        titulo: "Rosa do Deserto",
+        descricao: "A Rosa do Deserto (Adenium obesum) é uma suculenta conhecida por suas flores vibrantes e caudex espessado, que armazena água. Originária das regiões áridas da África, ela é uma planta de fácil cultivo e muito popular entre os colecionadores. Suas flores podem ser encontradas em diversas cores, como rosa, vermelho, branco e lilás, e costumam florescer durante a primavera e o verão.",
+        luz: "Sol pleno",
+        rega: "Moderada, esperando o solo secar entre as regas",
+        temperatura: "Quente e seco",
+        solo: "Bem drenado, com boa adição de areia",
+        adubacao: "A cada 2 meses, adubo rico em potássio",
+        poda: "Após a floração, removendo ramos secos",
+        moreinfo: "https://pt.wikipedia.org/wiki/Adenium_obesum",
+        tags: "suculentas grossa "
+    },
+    {
+        titulo: "Flor de Cadáver",
+        descricao: "A Flor de Cadáver (Amorphophallus titanum) é famosa por sua inflorescência gigante e odor fétido, semelhante ao de carne podre. Essa característica a ajuda a atrair polinizadores. Nativa das florestas tropicais de Sumatra, é considerada uma das maiores flores do mundo e uma verdadeira raridade.",
+        luz: "Sombra parcial",
+        rega: "Moderada, mas exige alta umidade ambiental",
+        temperatura: "Quente e úmida",
+        solo: "Rico em matéria orgânica, bem drenado",
+        adubacao: "A cada 3 meses, adubo equilibrado",
+        poda: "Após a floração, removendo a inflorescência",
+        moreinfo: "https://pt.wikipedia.org/wiki/Amorphophallus_titanum",
+        tags: "fedor grande assustadora"
+    }
+
+];
